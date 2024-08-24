4e923f187ddb405d354b6b06e6d3815df045cc48
sontiktok <sonvltiktok@gmail.com>
2024-08-24 11:29:56 +0700

    6.Component-componentScan

---
+++ src/main/java/com/example/componentscan/Employee.java
@@ -0,0 +1,53 @@
+package com.example.componentscan;
+
+import org.springframework.stereotype.Component;
+
+@Component("employee")
+public class Employee {
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String salary;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId='" + employeeId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary='" + salary + '\'' +
+                '}';
+    }
+}
